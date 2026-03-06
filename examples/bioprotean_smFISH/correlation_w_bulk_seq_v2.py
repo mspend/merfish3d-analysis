a4c19087a7d59fce5ecfde7720ac368a7ccb0ff7
mspend <mauridobbs@gmail.com>
2026-03-06 12:37:49 -0700

    adapt the bulk RNA seq correlation for kallisto results

--- examples/bioprotean_smFISH/correlation_w_bulk_seq_v2.py
+++ examples/bioprotean_smFISH/correlation_w_bulk_seq_v2.py
@@ -10,28 +10,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Read in bulk RNA-seq data 
-bulkseq_path = Path('/data/bulkRNA/IMG/cleanIMGcounts.csv')
+bulkseq_path = Path('/data/bulkRNA/IMG/kallisto/bulk_RNA_seq_counts_tpm.csv')
 bulkseq_df = pd.read_csv(bulkseq_path)
-bulkseq_counts = bulkseq_df.rename(columns={'Unnamed: 0':'gene_id', 'IMGbas1': 'counts', 'IMGbas2': 'counts_rep2'})
 
 # Select the relevant columns
-# The RNA sequencing data we have had multiple immune stimulants added to IMG cells, but we only want the columns titled 'IMGbas#', base meaning nothing added to these cells.
-bulkseq_counts = bulkseq_counts.drop(columns=['width', 'IMGlpc1', 'IMGlpc2', 'IMGlps1', 'IMGlps2', 'IMGpic1', 'IMGpic2', 'IMGpma1', 'IMGpma2'])
+bulkseq_counts = bulkseq_df.drop(columns=['Unnamed: 0'])
+bulkseq_counts = bulkseq_counts.rename(columns={'gene_name':'gene_id'})
+# print(bulkseq_counts.head())
 
 # Read in smFISH spots from the parquet file of decoded spots
 smfish_path = Path('/data/smFISH/20251028_bartelle_smFISH_mm_microglia_newbuffers/qi2labdatastore/all_tiles_filtered_decoded_features/decoded_features.parquet')
 
 smfish_df = pd.read_parquet(smfish_path)
+# print(smfish_df.head())
 
 # Obtain the number of counts per gene
 smfish_counts = smfish_df['gene_id'].value_counts().reset_index()
 smfish_counts = smfish_counts.rename(columns={'count': 'fish_spots'})
+# print(smfish_counts)
 
 # Merge the dataframes and make a scatter plot 
 merged_df = pd.merge(smfish_counts, bulkseq_counts, on='gene_id', how='left', indicator=True)
-plt.scatter(np.log10(merged_df['counts']),np.log10(merged_df['fish_spots']), alpha=0.5)
+print(merged_df)
+
+plt.scatter(np.log10(merged_df['tpm']),np.log10(merged_df['fish_spots']), alpha=0.5)
+# plt.scatter((merged_df['tpm']),(merged_df['fish_spots']), alpha=0.5)
 plt.ylabel('Log10(Number of FISH Spots Per Gene)')
-plt.xlabel('Log10(Raw RNA Counts)')
+plt.xlabel('Log10(TPM from bulk RNA-seq)')
 plt.title('Log-Log Correlation Plot')
 plt.grid(True)
 plt.show()
